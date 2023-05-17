a2b8835b0c89a57155a4d1967bb72e74a7c92457
Felipe Malengo <felipemalengo1@hotmail.com>
2023-05-17 00:17:48 -0300

    jogo da velha terminada

--- Velha/script.js
+++ Velha/script.js
@@ -15,6 +15,11 @@ const b2 = document.getElementById("b3");
 const c0 = document.getElementById("c1");
 const c1 = document.getElementById("c2");
 const c2 = document.getElementById("c3");
+const a = document.querySelector(".resultados");
+var delayInMilliseconds = 2000; //1 second
+
+//a0.addEventListener("click" , () => {
+//});
 
 
 
@@ -25,7 +30,7 @@ const c2 = document.getElementById("c3");
         if(jogador==1)  {
             bo.value="X"; 
             jogador=0;
-    
+            
     
         }
         
@@ -33,14 +38,49 @@ const c2 = document.getElementById("c3");
         else {
             bo.value="O"; 
             jogador=1;
-    
+            
+            
         }  
-    
+        
+        if(a0.value=="X" && a1.value=="X"&&a2.value=="X"||b0.value=="X" && b1.value=="X"&&b2.value=="X"||c0.value=="X" && c1.value=="X"&&c2.value=="X"||a0.value=="X" 
+        && b1.value=="X"&&c2.value=="X"||a2.value=="X" && b1.value=="X"&&c0.value=="X"||a0.value=="X" && b0.value=="X"&&c0.value=="X"||a1.value=="X" && b1.value=="X"&&c1.value=="X"||a2.value=="X" && b2.value=="X"&&C2.value=="X"){
+
+
+             var x = document.createElement("INPUT");
+             x.setAttribute("value", "X ganhou");
+             a.appendChild(x);
+
+             setTimeout(function() {
+                window.location.reload(true) 
+              }, delayInMilliseconds);
+           
+        }
+        
+        
+        if(a0.value=="O" && a1.value=="O"&&a2.value=="O"||b0.value=="O" && b1.value=="O"&&b2.value=="O"||c0.value=="O" && c1.value=="O"&&c2.value=="O"||a0.value=="O" && b1.value=="O"&&c2.value=="O"||a2.value=="O" 
+        && b1.value=="O"&&c0.value=="O"||a0.value=="O" && b0.value=="O"&&c0.value=="O"||a1.value=="O" && b1.value=="O"&&c1.value=="O"||a2.value=="O" && b2.value=="O"&&C2.value=="O") {
+      
+            var x = document.createElement("INPUT");
+            x.setAttribute("value", "O ganhou");
+            x.setAttribute("id", 1);
+            a.appendChild(x);
+            setTimeout(function() {
+                window.location.reload(true) 
+              }, delayInMilliseconds);
+
+        }
+        
+        
 
 }
 
+function calcular(){
+
+       
+   }
+
+
 
     
-
 
   
